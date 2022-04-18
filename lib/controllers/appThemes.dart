41db16f422b81dac9de33f916359b587b715b03b
Joshua Towell <jntowell@hotmail.com>
2022-04-18 15:02:35 +0100

    Made main changes to new light and dark themes for icons and text colours

--- lib/controllers/appThemes.dart
+++ lib/controllers/appThemes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/constants.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  ThemeMode themeMode = ThemeMode.dark;
+  ThemeMode themeMode = ThemeMode.system;
   bool get isDarkMode => themeMode == ThemeMode.dark;
 
   void toggleTheme() {
@@ -12,12 +12,34 @@ class ThemeProvider extends ChangeNotifier {
 }
 
 class AppThemes {
-  static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: backgroundColourDark,
-    colorScheme: const ColorScheme.dark(),
+  // ignore: prefer_const_constructors
+  static final TextTheme textTheme = TextTheme(
+    // headline1: TextStyle(),
   );
-  static final lightTheme = ThemeData(
+
+  static final ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: backgroundColourDark,
+    colorScheme: const ColorScheme.dark().copyWith(
+      primary: secondaryColourDark,
+      secondary: secondaryColourDark,
+    ),
+    iconTheme: const IconThemeData(color: primaryColourDark,),
+    textTheme: textTheme.apply(
+      bodyColor: secondaryColourDark,
+      displayColor: primaryColourDark,
+    ),
+  );
+
+  static final ThemeData lightTheme = ThemeData(
     scaffoldBackgroundColor: backgroundColourLight,
-    colorScheme: const ColorScheme.light(),
+    colorScheme: const ColorScheme.light().copyWith(
+      primary: secondaryColourLight,
+      secondary: secondaryColourLight,
+    ),
+    iconTheme: const IconThemeData(color: primaryColourLight,),
+    textTheme: textTheme.apply(
+      bodyColor: primaryColourLight,
+      displayColor: primaryColourLight,
+    ),
   );
 }
