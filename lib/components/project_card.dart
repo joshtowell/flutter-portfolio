876b225bac446a80b0d3d240916ddaa09830c5d8
Joshua Towell <jntowell@hotmail.com>
2022-06-05 21:29:00 +0100

    Added project object to projectCard to replace static data

--- lib/components/project_card.dart
+++ lib/components/project_card.dart
@@ -3,23 +3,17 @@ import 'package:flutter_portfolio/components/wide_card.dart';
 import 'package:provider/provider.dart';
 
 import '../constants.dart';
+import '../controllers/app_routes.dart';
 import '../controllers/app_themes.dart';
+import '../objects/project_object.dart';
 import 'elevating_button.dart';
 
 class ProjectCard extends StatelessWidget {
-  final Widget icon;
-  final String projectName;
-  final String projectCategory;
-  final String projectSummary;
-  final Widget? image;
+  final ProjectObject projectObject;
   final EdgeInsetsGeometry? padding;
 
   const ProjectCard({
-    required this.icon,
-    required this.projectName,
-    required this.projectCategory,
-    required this.projectSummary,
-    this.image,
+    required this.projectObject,
     this.padding,
     Key? key,
   }) : super(key: key);
@@ -39,24 +33,28 @@ class ProjectCard extends StatelessWidget {
                 hasShadow: false,
                 padding: const EdgeInsets.all(defaultPadding * 0.5,),
                 colour: themeProvider.isDarkMode ? backgroundColour2Dark : backgroundColour2Light,
-                child: icon,
+                child: projectObject.makeIcon(),
               ),
               const Icon(Icons.chevron_right_rounded, size: 40.0,)
             ],
           ),
           shoebox,
-          Text(projectName, style: headline1(context),),
+          Text(projectObject.projectName, style: headline1(context),),
           const SizedBox(height: defaultPadding * 0.25,),
-          Text(projectCategory, style: bodyText2(context)?.copyWith(color: backgroundColour3Light),),
+          Text(projectObject.projectType, style: bodyText2(context)?.copyWith(color: backgroundColour3Light),),
           shoebox,
-          Text(projectSummary, style: bodyText1(context),),
+          Text(projectObject.projectSummary, style: bodyText1(context),),
           const SizedBox(height: defaultPadding * 2,),
           Align(
             alignment: Alignment.bottomCenter,
-            child: image ?? Container(),
+            child: projectObject.makeImage(),
           ),
         ],
       ),
+      tapAction: () async {
+        final args = projectObject;
+        await Navigator.pushNamed(context, AppRoutes.workDetails, arguments: args,);
+      },
     );
   }
 }
