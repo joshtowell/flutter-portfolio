45183fb7601fdfaf979078cda34deb7cc109bfee
Joshua Towell <jntowell@hotmail.com>
2022-06-06 20:22:57 +0100

    Removed unnecessary imports

--- lib/views/landing_page.dart
+++ lib/views/landing_page.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/components/change_theme_widget.dart';
 import 'package:flutter_portfolio/components/navbar.dart';
